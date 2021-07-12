ef4ada4e9c20d436c87233651ca3ef076f57ba94
MdNaina <naina.stnm@gmail.com>
2021-07-12 15:35:46 +0530

    finished

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+const repo_url = "https://api.github.com/users/MdNaina/repos"
+const work__container = document.querySelector('.work__container')
+
+const get_repos = async () => {
+    let res = await fetch(repo_url);
+    if(res.ok){
+        datas = await res.json();
+        datas.forEach(data =>{
+            if(data['stargazers_count'] > 0){
+                console.log(data['name'])
+                let link = document.createElement('a')
+                link.href = data['svn_url']
+                link.classList.add('work__img')
+                link.innerHTML = `<img src="https://raw.githubusercontent.com/MdNaina/${data['name']}/master/screenshots/thumbnail.png" alt="${data['name']}">
+                    <div class="overlay">
+                        <div class="description">
+                            <h3>${data['name']}</h3> 
+                            <p>${data['description']}</p>
+                        </div>
+                    </div>`
+                work__container.appendChild(link)
+            }
+        })
+    }
+}
+
 const showMenu = (toggleId, navId) =>{
     nav = document.getElementById(navId)
     toggle = document.getElementById(toggleId)
@@ -37,3 +63,6 @@ const sr = ScrollReveal({
 sr.reveal(`.home__title, .button, .home__img, .home__social-icon, .about__text, .about__subtitle, .about__img, .skills__subtitle, .skills__text, .skills__img, .skills__data, .work__img, .contact__form`,{
     interval: 200
 })
+
+
+get_repos()
